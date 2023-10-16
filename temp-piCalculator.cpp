cf157b8b7bf96b9321c39e41619a5a18f8c24774
Iris Teyssier <iris.teyssier@gmail.com>
2023-10-16 17:10:43 +0000

    added outline for pi calclator

---
+++ temp-piCalculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    //don't allow user to enter number that is too big
+    //choose to find c or b(on arduino done by pressing enter on b)
+    //just have player enter "enter" for b
+    std::cout << "";
+    
+    //get a
+    //get b or c
+    //  C:
+    //square a and b
+    //[[fallthrough]]
+    //  B:
+    //square a and c
+    //subtract a from c
+    //see if you can find the root cleanly
+    //if not then simplify the root
+    //  simplify:
+    //find greatest rootable factor
+    //subtract it, then root it, then print it before the root
+}
