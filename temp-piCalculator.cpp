fb6df42f95441537a255f8ccfb472a2ecdef039a
Iris Teyssier <iris.teyssier@gmail.com>
2023-10-18 18:24:00 +0000

    added functionality to square numbers

--- temp-piCalculator.cpp
+++ temp-piCalculator.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
 
+//no input validation, this is a test
 int main()
 {
     //don't allow user to enter number that is too big
+    //account for errors
+    //don't allow negatives
     //choose to find c or b(on arduino done by pressing enter on b)
-    //just have player enter "enter" for b
-    std::cout << "";
+    std::cout << "A: ";
+    int64_t a{}; //64 bit int
+    std::cin >> a;
+    a = (a * a);
+
+    std::cout << "B: ";
+    int64_t b{};
+    std::cin >> b; //-27 is the temp value for not entering anything
+    b = (b * b);
+
+    int64_t c{};
+    int64_t x{};
+    bool findB{false};
+    if (b == (-27 * -27)) {
+        findB = true;
+        std::cout << "C: ";
+        std::cin >> c;
+        c = (c * c);
+        x = (c - a);
+    } else
+        x = (a + b);
     
-    //get a
-    //get b or c
-    //  C:
-    //square a and b
-    //[[fallthrough]]
-    //  B:
-    //square a and c
-    //subtract a from c
-    //see if you can find the root cleanly
-    //if not then simplify the root
-    //  simplify:
-    //find greatest rootable factor
-    //subtract it, then root it, then print it before the root
+    //check if x is a square
+    //if so, root it
+
+    //check if any of the factors of x are squares
+    //make sure we have the biggest rootable factor
+    //if so then subtract them from x print them before x
+
+    //if not then just pint x
 }
