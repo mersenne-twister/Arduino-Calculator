df29add69e07a44d2347b52baed2d4bb88e07e5c
Iris Teyssier <iris.teyssier@gmail.com>
2023-10-20 18:22:51 +0000

    added cleanSqrt

--- temp-piCalculator.cpp
+++ temp-piCalculator.cpp
@@ -1,4 +1,8 @@
+#include <cstdint>
 #include <iostream>
+#include "temp-factorCalculator.h"
+
+std::int64_t cleanSqrt(int num);
 
 //no input validation, this is a test
 int main()
@@ -8,17 +12,17 @@ int main()
     //don't allow negatives
     //choose to find c or b(on arduino done by pressing enter on b)
     std::cout << "A: ";
-    int64_t a{}; //64 bit int
+    std::int64_t a{}; //64 bit int
     std::cin >> a;
     a = (a * a);
 
     std::cout << "B: ";
-    int64_t b{};
+    std::int64_t b{};
     std::cin >> b; //-27 is the temp value for not entering anything
     b = (b * b);
 
-    int64_t c{};
-    int64_t x{};
+    std::int64_t c{};
+    std::int64_t x{};
     bool findB{false};
     if (b == (-27 * -27)) {
         findB = true;
@@ -29,8 +33,11 @@ int main()
     } else
         x = (a + b);
     
-    //check if x is a square
-    //if so, root it
+    if (cleanSqrt(x)) {
+        std::cout << cleanSqrt(x) << '\n';
+        return 0;
+    }
+
     //check if any of the factors of x are squares
     //modify factor function to find number of factors,
     //and then be able to get a given factor based off their number
@@ -39,4 +46,22 @@ int main()
     //if so then subtract them from x print them before x
 
     //if not then just print x
+
+    return 0;
+}
+
+//returns sqrt, or 0 if it can't cleanly be rooted
+std::int64_t cleanSqrt(int num)
+{
+    for (int i = 1; i*i <= num; ++i) {
+        if ((i * i) == num)
+            return i;
+    }
+    return 0;
+}
+
+//returns the sqrt, with up to [8?(for the lcd screen)] digits of precision
+double decSqrt(int num)
+{
+    return 0.0;
 }
