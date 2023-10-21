054ad550dd698622988f479b239575edc7fdcd4f
Iris Teyssier <iris.teyssier@gmail.com>
2023-10-21 12:16:38 -0700

    fikxed button cycling

--- mainMenu.ino
+++ mainMenu.ino
@@ -1,6 +1,4 @@
-void mainMenu()
-{
-  String const menus[8] = {
+String const menus[10] = { //this would be constexpr but apparently arduino strings can't be
     "   Arithmetic", "   Calculator", //leading spaces for center allignment
     "  Factor Pair", "   Calculator",
     "Pythagorean theo", "   Calculator",
@@ -8,14 +6,11 @@ void mainMenu()
     "Knuth's Up Arrow", "Notat Calculator"
   };
 
+void mainMenu()
+{
   int currentMenu{0};
-
+  writeMenu(currentMenu);
   while (true) {
-    lcd.clear();
-    lcd.print(menus[currentMenu]);
-    lcd.setCursor(0, 1); //display the bottom half
-    lcd.print(menus[currentMenu += 1]);
-
     bool buttonPressed{false};
     while (!buttonPressed) {
       if (digitalRead(enterButton)) {
@@ -39,15 +34,22 @@ void mainMenu()
           while (true) {}
           break; //wont' trigger, but break for posterity
         }
+
+        writeMenu(currentMenu);
       }
 
       if (digitalRead(deleteButton)) {
-        delay(50); //debounce delay
-        
-        currentMenu += 2;
-        if (currentMenu > 10)
-        currentMenu = 0;
         buttonPressed = true;
+        delay(50); //debounce delay
+
+        currentMenu += 2;
+        
+        if (currentMenu > 9)
+        currentMenu = 0;
+
+        writeMenu(currentMenu);
+        while (digitalRead(deleteButton)) {} //wait until they lift the button up
+        delay(50);
       }
     }
 
@@ -61,4 +63,12 @@ void mainMenu()
 
 
 
+}
+
+void writeMenu(int currentMenu)
+{
+  lcd.clear();
+  lcd.print(menus[currentMenu]);
+  lcd.setCursor(0, 1); //display the bottom half
+  lcd.print(menus[currentMenu + 1]);
 }
