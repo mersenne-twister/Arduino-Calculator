b872364b28d230d2095ca3a6e0c47f6ec9c4673c
Iris Teyssier <iris.teyssier@gmail.com>
2023-10-20 22:58:58 -0700

    addd mainmenu.ino

---
+++ mainMenu.ino
@@ -0,0 +1,16 @@
+void mainMenu()
+{
+  String const menus[8] = {
+    "Arithmetic Calcu", "lator",
+    "Factor Pair Calc", "ulator",
+    "Pythagorean th C", "alculator",
+    "Quadratic fo Cal", "culator"
+  };
+
+  //shutdown if back
+  //select if enter
+  //cycle if delete
+
+
+
+}
