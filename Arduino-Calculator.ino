8ed4416aabd4333c1825aacabde20cab1b864a27
Iris Teyssier <iris.teyssier@gmail.com>
2023-10-20 22:42:25 -0700

    set up buttons

--- Arduino-Calculator.ino
+++ Arduino-Calculator.ino
@@ -1,6 +1,11 @@
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
 
+int constexpr backButton{  10}; //backButton is leftmost button
+int constexpr enterButton{ 11};
+int constexpr clearButton{ 12};
+int constexpr deleteButton{13};
+
 const byte ROWS = 4; 
 const byte COLS = 4; 
 
@@ -10,7 +15,7 @@ char keys[ROWS][COLS] = {
 {'1', '2', '3', '/'},
 {'4', '5', '6', '*'},
 {'7', '8', '9', '-'},
-{'-', '0', '.', '+'}
+{'_', '0', '.', '+'} //underscore represents negative instead of subtraction, and will still print out '-'
 };
 
 byte rowPins[ROWS] = {9, 8, 7, 6}; 
@@ -19,6 +24,12 @@ byte colPins[COLS] = {5, 4, 3, 2};
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
+  pinMode(backButton, INPUT);
+  pinMode(enterButton, INPUT);
+  pinMode(clearButton, INPUT);
+  pinMode(deleteButton, INPUT);
+
+
   Serial.begin(9600);
   
   lcd.init();
@@ -32,8 +43,45 @@ void setup() {
 }
 
 void loop() {
-  char pressedKey = keypad.getKey();
+  if (digitalRead(backButton)) {
+    lcd.setCursor(0, 0);
+    lcd.print("1");
+    Serial.println("back on");
+  } else {
+    lcd.setCursor(0, 0);
+    lcd.print(" ");
+  }
 
-  if (pressedKey)
-    lcd.print(pressedKey);
+  if (digitalRead(enterButton)) {
+    lcd.setCursor(1, 0);
+    lcd.print("2");
+    Serial.println("enter on");
+  } else {
+    lcd.setCursor(1, 0);
+    lcd.print(" ");
+  }
+
+  if (digitalRead(clearButton)) {
+    lcd.setCursor(2, 0);
+    lcd.print("3");
+    Serial.println("clear on");
+  } else {
+    lcd.setCursor(2, 0);
+    lcd.print(" ");
+  }
+
+  if (digitalRead(deleteButton)) {
+    lcd.setCursor(3, 0);
+    lcd.print("4");
+    Serial.println("delete on");
+  } else {
+    lcd.setCursor(3, 0);
+    lcd.print(" ");
+  }
+
+  //delay(50);
+  // char pressedKey = keypad.getKey();
+
+  // if (pressedKey)
+  //   lcd.print(pressedKey);
 }
