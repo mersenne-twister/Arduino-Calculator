d1131ddd71f7abfee98f5ce4bbb47bc66d0a7970
Iris Teyssier <iris.teyssier@gmail.com>
2023-10-21 11:54:23 -0700

    added basic main menu functionality

--- mainMenu.ino
+++ mainMenu.ino
@@ -1,13 +1,61 @@
 void mainMenu()
 {
   String const menus[8] = {
-    "Arithmetic Calcu", "lator",
-    "Factor Pair Calc", "ulator",
-    "Pythagorean th C", "alculator",
-    "Quadratic fo Cal", "culator"
+    "   Arithmetic", "   Calculator", //leading spaces for center allignment
+    "  Factor Pair", "   Calculator",
+    "Pythagorean theo", "   Calculator",
+    " Quadratic Form", "   Calculator",
+    "Knuth's Up Arrow", "Notat Calculator"
   };
 
-  //shutdown if back
+  int currentMenu{0};
+
+  while (true) {
+    lcd.clear();
+    lcd.print(menus[currentMenu]);
+    lcd.setCursor(0, 1); //display the bottom half
+    lcd.print(menus[currentMenu += 1]);
+
+    bool buttonPressed{false};
+    while (!buttonPressed) {
+      if (digitalRead(enterButton)) {
+        delay(50);
+
+        switch (currentMenu) {
+        case 0:
+
+        case 2:
+
+        case 4:
+
+        case 6:
+
+        case 8:
+        
+        case 10:
+        default:
+          lcd.clear();
+          lcd.print("ERROR BAD MENU");
+          while (true) {}
+          break; //wont' trigger, but break for posterity
+        }
+      }
+
+      if (digitalRead(deleteButton)) {
+        delay(50); //debounce delay
+        
+        currentMenu += 2;
+        if (currentMenu > 10)
+        currentMenu = 0;
+        buttonPressed = true;
+      }
+    }
+
+
+
+
+  }
+
   //select if enter
   //cycle if delete
 
