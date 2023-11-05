cfa9825da68e6d7210528c5fcde45b6e6a7e719d
Iris Teyssier <iris.teyssier@gmail.com>
2023-11-04 20:37:06 -0700

    migrate files

---
+++ io.h
@@ -0,0 +1,16 @@
+#ifndef IO_201604112023_H
+#define IO_201604112023_H
+
+#include <Arduino.h>
+
+void print(String topText, String bottomText = "");
+
+bool input(int const button);
+
+void waitForButton(int button);
+
+int validateNumInput(int pressedKey, int enteredNum);
+
+int getNumInput(bool& backPressed);
+
+#endif //IO_201604112023_H
