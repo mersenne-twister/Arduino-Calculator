1b9e31794e2eaf38c2303d7a05e7f779e4af25c7
Iris Teyssier <iris.teyssier@gmail.com>
2023-11-04 21:23:20 -0700

    implement changes

--- io.cpp
+++ io.cpp
@@ -12,36 +12,35 @@ void print(String topText, String bottomText = "")
 //TODO: replace all input with this function
 bool input(int button)
 {
+    if ((button < 2) || (button > 13))
+        return false;
+
     if (!digitalRead(button))
         return false;
 
     delay(50);
-    while (digitalRead(button)) {}
+    while (digitalRead(button));
     delay(50);
     return true;
 }
 
-void waitForButton(int button)
+void waitForButton(int button0, int button1 = 0, int button2 = 0, int button3 = 0)
 {
-    while (!digitalRead(button)) {}
-    delay(50);
-    while (digitalRead(button)) {}
-    delay(50);
+    while (!input(button0) || !input(button1) || !input(button2) || !input(button3));
+    //while (!digitalRead(button));
+    //delay(50);
+    //while (digitalRead(button));
+    //delay(50);
 }
 
-int validateNumInput(int pressedKey, int enteredNum)
+int validateNumInput(char pressedKey, unsigned int enteredNum)
 {
     int pressedNum = pressedKey - 48; //subtracts 48 to get the ascii digits down to their actual values(0 is ascii 48, etc)
     if ((pressedNum >= 0) && (pressedNum <= 9) && !(!enteredNum && !pressedNum)) {
         if (enteredNum > ((65535 - pressedNum) / 10)) { //arduino is weird with ints more then 16 bit, so don't allow them
             print("Max num size");
 
-            while (!digitalRead(enterButton)) {
-            }
-            delay(50);
-            while (digitalRead(enterButton)) {
-            }
-            delay(50);
+            waitForButton(enterButton);
 
             lcd.clear();
             lcd.print(enteredNum);
@@ -55,7 +54,7 @@ int validateNumInput(int pressedKey, int enteredNum)
     return enteredNum;
 }
 
-int getNumInput(bool& backPressed) //I reallyyyyyy hope this works
+int getNumInput(bool& backPressed)
 {
     lcd.clear();
     uint16_t enteredNum{0};
@@ -64,52 +63,20 @@ int getNumInput(bool& backPressed) //I reallyyyyyy hope this works
 
         char pressedKey = keypad.getKey();
         enteredNum = validateNumInput(pressedKey, enteredNum);
-        // int pressedNum = pressedKey - 48; //subtracts 48 to get the ascii digits down to their actual values(0 is ascii 48, etc)
-        // if ((pressedNum >= 0) && (pressedNum <= 9) && !(!enteredNum && !pressedNum)) {
-        //     if (enteredNum > ((65535 - pressedNum) / 10) ) { //arduino is weird with ints more then 16 bit, so don't allow them
-        //         lcd.clear();
-        //         lcd.print("Max num size");
-
-        //         while (!digitalRead(enterButton)) {}
-        //         delay(50);
-        //         while (digitalRead(enterButton)) {}
-        //         delay(50);
-
-        //         lcd.clear();
-        //         lcd.print(enteredNum);
-        //     } else {
-        //         enteredNum = (enteredNum * 10) + pressedNum;
-        //         lcd.clear();
-        //         lcd.print(enteredNum);
-        //     }
-        // }
 
         if (pressedKey)
             delay(50); //debounce delay
 
         if (input(backButton))
             backPressed = true;
-        // if (digitalRead(backButton)) {
-        //     delay(50);
-        //     backPressed = true;
-        // }
+
         if (input(enterButton)) {
-        //if (digitalRead(enterButton)) { //enteredNum has to be not 0
-            //while (digitalRead(enterButton)) {}
+
             if (enteredNum) {
-            //delay(50);
                 break;
             } else {
                 print("Must enter", "number");
-                // lcd.clear();
-                // lcd.print("Must enter");
-                // lcd.setCursor(0, 1);
-                // lcd.print("number");
-
                 waitForButton(enterButton);
-                // while (!digitalRead(enterButton)) {}
-                // while (digitalRead(enterButton)) {}
-                // delay(50);
                 lcd.clear();
                 lcd.print(enteredNum);
             }
@@ -127,7 +94,7 @@ int getNumInput(bool& backPressed) //I reallyyyyyy hope this works
 
         if (digitalRead(deleteButton)) {
             delay(50);
-            enteredNum /= 10; //integer division
+            enteredNum /= 10;
             lcd.clear();
             lcd.print(enteredNum);
             while (digitalRead(deleteButton)) {
