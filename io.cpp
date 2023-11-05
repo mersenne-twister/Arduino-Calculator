a4f631758490a05fe8c2fee902b9d0760c1e90c3
Iris Teyssier <iris.teyssier@gmail.com>
2023-11-05 11:11:48 -0800

    fix indents

--- io.cpp
+++ io.cpp
@@ -6,101 +6,99 @@
 //TOOD: replace all lcd interaction with this function
 void write(String topText, String bottomText = "")
 {
-    lcd.clear();
-    lcd.print(topText);
-    lcd.setCursor(0, 1);
-    lcd.print(bottomText);
+	lcd.clear();
+	lcd.print(topText);
+	lcd.setCursor(0, 1);
+	lcd.print(bottomText);
 }
 
 void write(unsigned int topText)
 {
-    lcd.clear();
-    lcd.print(topText);
+	lcd.clear();
+	lcd.print(topText);
 }
 
 //TODO: replace all input with this function
 bool input(int button)
 {
-    if ((button < 2) || (button > 13) || !digitalRead(button))
-        return false;
+	if ((button < 2) || (button > 13) || !digitalRead(button))
+		return false;
 
-    delay(50);
-    while (digitalRead(button));
-    delay(50);
-    return true;
+	delay(50);
+	while (digitalRead(button));
+	delay(50);
+	return true;
 }
 
 int isInput()
 {
-    if (input(backButton))
-        return backButton;
-    else if (input(enterButton))
-        return enterButton;
-    else if (input(clearButton))
-        return clearButton;
-    else if (input(deleteButton))
-        return deleteButton;
-    return 0;
+	if (input(backButton))
+		return backButton;
+	else if (input(enterButton))
+		return enterButton;
+	else if (input(clearButton))
+		return clearButton;
+	else if (input(deleteButton))
+		return deleteButton;
+	return 0;
 }
-
-
 
 void waitForButton(int button0, int button1 = 0, int button2 = 0, int button3 = 0)
 {
-    while (!input(button0) || !input(button1) || !input(button2) || !input(button3));
+	while (!input(button0) || !input(button1) || !input(button2) || !input(button3));
 }
 
 int validateNumInput(char pressedKey, unsigned int enteredNum)
 {
-    int pressedNum = (pressedKey - '0');
-    if ((pressedNum >= 0) && (pressedNum <= 9) && !(!enteredNum && !pressedNum)) {
-        if (enteredNum > ((65535 - pressedNum) / 10)) { //arduino is weird with ints more then 16 bit, so don't allow them
-            write("Max num size");
-            waitForButton(enterButton);
-        } else {
-            enteredNum = (enteredNum * 10) + pressedNum;
-        }
-        write(enteredNum);
-    }
+	int pressedNum = (pressedKey - '0');
+	if ((pressedNum >= 0) && (pressedNum <= 9) && !(!enteredNum && !pressedNum)) {
+		if (enteredNum > ((65535 - pressedNum) / 10)) { //arduino is weird with ints more then 16 bit, so don't allow them
+			write("Max num size");
+			waitForButton(enterButton);
+		} else {
+			enteredNum = (enteredNum * 10) + pressedNum;
+		}
+		write(enteredNum);
+	}
 
-    return enteredNum;
+	return enteredNum;
 }
 
 int getNumInput(bool& backPressed)
 {
-    uint16_t enteredNum{0};
-    write(enteredNum);
-    while (!backPressed) {
+	uint16_t enteredNum{0};
+	write(enteredNum);
+	while (!backPressed) {
 
-        char pressedKey = keypad.getKey();
-        enteredNum = validateNumInput(pressedKey, enteredNum);
+		char pressedKey = keypad.getKey();
+		enteredNum = validateNumInput(pressedKey, enteredNum);
 
-        if (pressedKey)
-            delay(50); //debounce delay
+		if (pressedKey)
+			delay(50); //debounce delay
 
-        switch (isInput()) {
-        case backButton:
-            backPressed = true;
-            return 1; //would return 0 but that might break something
-        case enterButton:
-            if (enteredNum) {
-                return enteredNum;
-            } else {
-                write("Must enter", "number");
-                waitForButton(enterButton);
-                write(enteredNum);
-            }
-            break;
-        case clearButton:
-            enteredNum = 0;
-            write(enteredNum);
-            break;
-        case deleteButton:
-            enteredNum /= 10;
-            write(enteredNum);
-            break;
-        default:
-            break;
-        }
-    }
+		switch (isInput()) {
+		case backButton:
+			backPressed = true;
+			return 1; //would return 0 but that might break something
+		case enterButton:
+			if (enteredNum) {
+				return enteredNum;
+			} else {
+				write("Must enter", "number");
+				waitForButton(enterButton);
+				write(enteredNum);
+			}
+			break;
+		case clearButton:
+			enteredNum = 0;
+			write(enteredNum);
+			break;
+		case deleteButton:
+			enteredNum /= 10;
+			write(enteredNum);
+			break;
+		default:
+			break;
+		}
+	}
 }
