29f6bc748eef1f4a632dc5a198d8f72e551d77d5
Iris Teyssier <iris.teyssier@gmail.com>
2023-11-05 11:11:34 -0800

    migrate funcs

--- math.cpp
+++ math.cpp
@@ -1,1 +1,39 @@
 #include <Arduino.h>
+
+//TODO: use factor pair struct
+void printFactors(int factor, int page)
+{ //max number we would need to display is 5 digits(65535), so we split the space up in groups of 5
+    page *= 6; //so we only have to increase page by one in the logic above
+    lcd.clear();
+
+    for (int i = 0; i < 6; ++i) {
+        lcd.setCursor((i / 2 * 5), (i % 2));
+        if (getFactor(factor, (page + i)))
+            lcd.print(getFactor(factor, (page + i)));
+    }
+}
+
+int numFactors(int factor)
+{
+    int amountFactors{0};
+    for (int i = 1; (i <= (factor / 2)) && (i <= (factor / i)); ++i) {
+        if (!(factor % i)) {
+            amountFactors += 2;
+        }
+    }
+    return amountFactors;
+}
+
+uint getFactor(uint16_t composite, int numFactor)
+{
+    int currentFactor{-1}; //causes the first valid factor to set it to zero, so we can start from 0 instead of 1
+    for (uint i = 1; (i <= (composite / 2)) && (i <= (composite / i)); ++i) {
+        if (!(composite % i)) {
+            if (++currentFactor == numFactor) //i is the smaller factor
+                return i;
+            else if (++currentFactor == numFactor)
+                return (composite / i);
+        }
+    }
+    return 0;
+}
---
+++ math.h
@@ -0,0 +1,10 @@
+#ifndef MATH_111005112023_H
+#define MATH_111005112023_H
+
+void printFactors(int factor, int page);
+
+int numFactors(int factor);
+
+unsigned int getFactor(uint16_t composite, int numFactor);
+
+#endif //MATH_111005112023_H
