2d38914af3e2d39edbf4bfc78f590f84a66fab9a
Iris Teyssier <iris.teyssier@gmail.com>
2023-11-05 13:05:43 -0800

    refactor

--- calculators.cpp
+++ calculators.cpp
@@ -1,51 +1,47 @@
 #include <Arduino.h>
+#include "calculators.h"
 #include "io.h"
 #include "main.h"
 #include "math.h"
 
 void factPairCalc()
 {
-    bool backPressed{false}; //lets us break out of the outer while loop from inside functions
-    while (!backPressed) {
+	bool backPressed{false}; //lets us break out of the outer while loop from inside functions
+	while (!backPressed) {
 
-        uint16_t enteredNum{0};
-        enteredNum = getNumInput(backPressed);
+		unsigned int enteredNum{0};
+		enteredNum = getNumInput(backPressed);
 
-        int page{0};
-        printFactors(enteredNum, page);
+		factorsMenu(backPressed, enteredNum);
+	}
+}
 
-        while (!backPressed) {
+void factorsMenu(bool& backPressed, unsigned int enteredNum)
+{
+	int page{0};
+	printFactors(enteredNum, page);
 
-            if (digitalRead(deleteButton)) {
-                delay(50);
-                ++page;
-                if (page > ((numFactors(enteredNum) - 1) / 6))
-                    page = 0;
-                printFactors(enteredNum, page);
-                while (digitalRead(deleteButton)) {
-                }
-                delay(50);
-            }
-
-            if (digitalRead(clearButton) || digitalRead(enterButton)) {
-                delay(50);
-                lcd.clear();
-                enteredNum = 0;
-                lcd.print(enteredNum);
-                while (digitalRead(clearButton) || digitalRead(enterButton)) {
-                }
-                delay(50);
-                break;
-            }
-
-            if (digitalRead(backButton)) {
-                delay(50);
-                while (digitalRead(backButton)) {
-                }
-                backPressed = true;
-            }
-        }
-    }
+	while (!backPressed) {
+		switch (isInput()) {
+		case backButton:
+			backPressed = true;
+			break;
+		case enterButton:
+		case clearButton:
+			enteredNum = 0;
+			write(enteredNum);
+			waitForButton(enterButton, clearButton);
+			return; //breaks out of the inner loop, but not the outer one
+		case deleteButton:
+			++page;
+			if (page > ((numFactors(enteredNum) - 1) / 6))
+				page = 0;
+			printFactors(enteredNum, page);
+			break;
+		default:
+			break;
+		}
+	}
 }
 
 int quadCalc()
