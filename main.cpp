fb365ce288813d5c3380c9d47d1457b95b957233
Iris Teyssier <iris.teyssier@gmail.com>
2023-11-04 20:49:16 -0700

    move mainmenu contents into main

--- main.cpp
+++ main.cpp
@@ -35,7 +35,90 @@ void setup() {
 }
 
 void loop() {
-    mainMenu(); //TODO: move main menu stuff into void loop
+	String const menus[16] = { //this would be constexpr but apparently arduino strings can't be
+	" Ex Arithmetic", "   Calculator", //leading spaces for center allignment
+	" Dec Arithmetic", "   Calculator",
+	"  Factor Pair", "   Calculator",
+	"  Square Root", "   Simplifier",
+	"  Square Root", "   Calculator",
+	"    Fraction", "   Simplifier",
+	"Pythagorean Theo", "   Calculator",
+	//hook up the root calcs
+	" Quadratic Form", "   Calculator",
+	//hook up root calc
+	};
+
+	int currentMenu{0};
+	print(menus[currentMenu], menus[currentMenu + 1]);
+	while (true) {
+		bool buttonPressed{false};
+		while (!buttonPressed) {
+			if (digitalRead(enterButton)) {
+				delay(50);
+				while (digitalRead(enterButton));
+
+				switch (currentMenu) {
+				case 0:
+					//ex arithmetic calc
+				case 2:
+					//dec arithmetic calc
+				case 4:
+					//factor pair calc
+				case 6:
+					//sqrt simpl
+				case 8:
+					//sqrt calc
+				case 10:
+					//fract simpl
+				case 12:
+					//pyth theo calc
+				case 14:
+					//quad form calc
+				default:
+					lcd.clear();
+					lcd.print("ERROR BAD MENU");
+					while (true);
+				}
+
+				print(menus[currentMenu], menus[currentMenu + 1]);
+			}
+
+			if (digitalRead(deleteButton)) {
+				buttonPressed = true;
+				delay(50); //debounce delay
+
+				currentMenu += 2;
+
+				if (currentMenu > 12)
+					currentMenu = 0;
+
+				print(menus[currentMenu], menus[currentMenu + 1]);
+				while (digitalRead(deleteButton)); //wait until they lift the button up
+				delay(50);
+			}
+
+			if (digitalRead(clearButton)) {
+				buttonPressed = true;
+				delay(50); //debounce delay
+
+				currentMenu -= 2;
+
+				if (currentMenu < 0)
+					currentMenu = 12;
+
+				print(menus[currentMenu], menus[currentMenu + 1]);
+				while (digitalRead(clearButton)); //wait until they lift the button up
+				delay(50);
+			}
+		}
+
+
+
+
+	}
+
+	//select if enter
+	//cycle if delete
 
     //TODO: divide by zero thought you were slick didn't ya shutting downc:\Users\Jamie\source\repos\tests\Arduino-Calculator\Arduino-Calculator.ino
     //divide by zero unlocks secret mode??
