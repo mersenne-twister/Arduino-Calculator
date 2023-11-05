77da1c50702c8455dda28bdb462d22bbab638514
Iris Teyssier <iris.teyssier@gmail.com>
2023-11-04 18:06:45 -0700

    add basic header structure

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+#include "main.h"
+
+void setup() {
+    pinMode(backButton, INPUT);
+    pinMode(enterButton, INPUT);
+    pinMode(clearButton, INPUT);
+    pinMode(deleteButton, INPUT);
+
+
+    Serial.begin(9600);
+  
+    lcd.init();
+    lcd.clear();
+    lcd.backlight();
+}
+
+void loop() {
+    //mainMenu(); //TODO: move main menu stuff into void loop
+
+    //TODO: divide by zero thought you were slick didn't ya shutting downc:\Users\Jamie\source\repos\tests\Arduino-Calculator\Arduino-Calculator.ino
+    //divide by zero unlocks secret mode??
+
+
+}
+
+//TODO: switch while loops to semicolon
+
+//likely won't use this
+////enter the expression to assert, and the same expression in quotes so we can find the assertion
+//void assert(bool isValid, String assertion)
+//{
+//    if (!isValid) {
+//        lcd.clear();
+//        lcd.print("   ASSERTION");
+//        lcd.setCursor(0, 1);
+//        lcd.print("     ERROR");
+//        Serial.print("ASSERTION ERROR: ");
+//        Serial.println(assertion);
+//    }
+//}
---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_175404112023_H
+#define MAIN_175404112023_H
+
+int constexpr backButton{10}; //backButton is leftmost button
+int constexpr enterButton{11};
+int constexpr clearButton{12};
+int constexpr deleteButton{13};
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+char keys[ROWS][COLS] = {
+{'1', '2', '3', '/'},
+{'4', '5', '6', '*'},
+{'7', '8', '9', '-'},
+{'_', '0', '.', '+'} //underscore represents negative instead of subtraction, and will still print out '-'
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3, 2};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+#endif //MAIN_175404112023_H
