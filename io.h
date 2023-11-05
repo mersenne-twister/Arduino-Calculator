2740527903939446d1e1b2d7ba1c9926cf730c01
Iris Teyssier <iris.teyssier@gmail.com>
2023-11-04 21:23:01 -0700

    add docs

--- io.h
+++ io.h
@@ -3,14 +3,19 @@
 
 #include <Arduino.h>
 
+//prints to the lcd
 void print(String topText, String bottomText = "");
 
+//returns a bool if there is input on a given pin, and handles debouncing
 bool input(int button);
 
+//waits for input on up to 4 pins, and handles debouncing
 void waitForButton(int button);
 
+//checks to see if adding pressedkey to enterednum would overflow 16 bits(unsigned)
 int validateNumInput(int pressedKey, int enteredNum);
 
+//gets unsigned integer input
 int getNumInput(bool& backPressed);
 
 #endif //IO_201604112023_H
