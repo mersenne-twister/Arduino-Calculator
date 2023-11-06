81d12bbd6befe31dbf3c52635213c912be17fdd6
Iris Teyssier <iris.teyssier@gmail.com>
2023-11-06 01:16:57 -0800

    add math stuff

--- math.cpp
+++ math.cpp
@@ -4,40 +4,71 @@
 #include "main.h"
 #include "math.h"
 
-//TODO: use factor pair struct
-void printFactors(unsigned factor, int page)
-{ //max number we would need to display is 5 digits(65535), so we split the space up in groups of 5
-    page *= 6; //so we only have to increase page by one in the logic above
-    lcd.clear();
+unsigned int cleanSqrt(unsigned int root)
+{
+	//add a while loop?
+	if (!root)
+		return root;
 
-    for (int i = 0; i < 6; ++i) {
-        lcd.setCursor((i / 2 * 6), (i % 2));
-        if (getFactor(factor, (page + i)))
-            lcd.print(getFactor(factor, (page + i)));
-    }
+	for (int i = 1; i * i <= root; ++i) {
+		if ((i * i) == root)
+			return i;
+		
+	}
+	return 65535; //65535 will never be the factor of a 16 bit int
 }
 
-int numFactors(unsigned int factor)
+Square simplifySquare(Square square)
 {
-    int amountFactors{0};
-    for (unsigned int i = 1; (i <= (factor / 2)) && (i <= (factor / i)); ++i) {
-        if (!(factor % i)) {
-            amountFactors += 2;
-        }
-    }
-    return amountFactors;
+	if (cleanSqrt(square.root) != 65535) {
+		square.coef *= cleanSqrt(square.root);
+		square.root = 1;
+	} else {
+		for (unsigned int i = (numFactors(square.root) - 1); i > 2; --i) {
+			unsigned int factor{getFactor(square.root, (i))};
+			if (!(square.root % factor) && (cleanSqrt(factor) != 65535)) {
+				square.coef *= cleanSqrt(factor);
+				square.root /= factor;
+			}
+		}
+	}
+	return square;
+}
+
+//TODO: use factor pair struct
+void printFactors(unsigned int factor, int page)
+{ //max number we would need to display is 5 digits(65535), so we split the space up in groups of 5
+	page *= 6; //so we only have to increase page by one in the logic above
+	lcd.clear();
+
+	for (int i = 0; i < 6; ++i) {
+		lcd.setCursor((i / 2 * 6), (i % 2));
+		if (getFactor(factor, (page + i)))
+			lcd.print(getFactor(factor, (page + i)));
+	}
+}
+
+unsigned int numFactors(unsigned int factor)
+{
+	int amountFactors{0};
+	for (unsigned int i = 1; (i <= (factor / 2)) && (i <= (factor / i)); ++i) {
+		if (!(factor % i)) {
+			amountFactors += 2;
+		}
+	}
+	return amountFactors;
 }
 
 unsigned int getFactor(unsigned int composite, int numFactor)
 {
-    int currentFactor{-1}; //causes the first valid factor to set it to zero, so we can start from 0 instead of 1
-    for (unsigned int i = 1; (i <= (composite / 2)) && (i <= (composite / i)); ++i) {
-        if (!(composite % i)) {
-            if (++currentFactor == numFactor) //i is the smaller factor
-                return i;
-            else if (++currentFactor == numFactor)
-                return (composite / i);
-        }
-    }
-    return 0;
+	int currentFactor{-1}; //causes the first valid factor to set it to zero, so we can start from 0 instead of 1
+	for (unsigned int i = 1; (i <= (composite / 2)) && (i <= (composite / i)); ++i) {
+		if (!(composite % i)) {
+			if (++currentFactor == numFactor) //i is the smaller factor
+				return i;
+			else if (++currentFactor == numFactor)
+				return (composite / i);
+		}
+	}
+	return 0;
 }
