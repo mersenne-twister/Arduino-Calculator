86f00478f377ac9166c6eda446c26bb5d1687026
Iris Teyssier <iris.teyssier@gmail.com>
2023-11-06 01:17:17 -0800

    add funcs

--- io.cpp
+++ io.cpp
@@ -18,6 +18,21 @@ void write(unsigned int topText)
 	lcd.print(topText);
 }
 
+void write(unsigned int arg0, char arg1, unsigned int arg2)
+{
+	lcd.clear();
+	lcd.print(arg0);
+	lcd.print(arg1);
+	lcd.print(arg2);
+}
+
+void write(char arg0, unsigned int arg1)
+{
+	lcd.clear();
+	lcd.print(arg0);
+	lcd.print(arg1);
+}
+
 //TODO: replace all input with this function
 bool input(int button)
 {
@@ -48,55 +63,17 @@ void waitForButton(int button0, int button1 = 0, int button2 = 0, int button3 = 
 	while (!(input(button0) || input(button1) || input(button2) || input(button3)));
 }
 
-int validateNumInput(char pressedKey, unsigned int enteredNum)
+void idleMenu(bool& backPressed)
 {
-	int pressedNum = (pressedKey - '0');
-	if ((pressedNum >= 0) && (pressedNum <= 9) && !(!enteredNum && !pressedNum)) {
-		if (enteredNum > ((65535 - pressedNum) / 10)) { //arduino is weird with ints more then 16 bit, so don't allow them
-			write("Max num size");
-			waitForButton(enterButton);
-		} else {
-			enteredNum = (enteredNum * 10) + pressedNum;
-		}
-		write(enteredNum);
-	}
-
-	return enteredNum;
-}
-
-int getNumInput(bool& backPressed)
-{
-	unsigned int enteredNum{0};
-	write(enteredNum);
 	while (!backPressed) {
-
-		char pressedKey = keypad.getKey();
-		enteredNum = validateNumInput(pressedKey, enteredNum);
-
-		if (pressedKey)
-			delay(50); //debounce delay
-
 		switch (isInput()) {
 		case backButton:
 			backPressed = true;
-			return 1; //would return 0 but that might break something
+			break;
 		case enterButton:
-			if (enteredNum) {
-				return enteredNum;
-			} else {
-				write("Must enter", "number");
-				waitForButton(enterButton);
-				write(enteredNum);
-			}
-			break;
 		case clearButton:
-			enteredNum = 0;
-			write(enteredNum);
-			break;
+			return;
 		case deleteButton:
-			enteredNum /= 10;
-			write(enteredNum);
-			break;
 		default:
 			break;
 		}
