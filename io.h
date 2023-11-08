f1868c86bb2f649e92e466746b5132d7fdc8f755
Iris Teyssier <iris.teyssier@gmail.com>
2023-11-07 21:38:05 -0800

    deleted prototype

--- io.h
+++ io.h
@@ -17,8 +17,6 @@ bool input(int button);
 //returns the pin number of any activated pin
 int isInput();
 
-void overflows(bool& backPressed, String errMessage = "try again");
-
 //waits for input on up to 4 pins, and handles debouncing
 void waitForButton(int button0, int button1 = 0, int button2 = 0, int button3 = 0);
 
